16973114699673ad51c5e5fd9a3721ea6fde44a4
Luntu <luntu.mavukz35@gmail.com>
2016-08-05 15:07:31 +0200

    iterating using keyboard

--- iOS/Views/LoginViewController.cs
+++ iOS/Views/LoginViewController.cs
@@ -33,64 +33,40 @@ namespace HookMeUP.iOS
 
 			loginButton.TouchUpInside += async (sender, evt) =>
 		   	{
-			   i++;
-			 
+				i++;
 
 			   switch (!usernameText.Text.Equals("") && !passwordText.Text.Equals(""))
 			   {
 
 				case true:
-						   	var query = from userInformation in ParseObject.GetQuery("UserInformation")
-						   				where userInformation.Get<string>("Username") == usernameText.Text
-						   				select userInformation;
-
-						   	IEnumerable<ParseObject> results = await query.FindAsync();
-							foreach (var elements in results)
-							{
-							   string n = elements.Get<string>("Name");
-							   System.Diagnostics.Debug.WriteLine(n);
-							}
-						/*
 						   try
-					   {
+						   {
 							
 
-							//	ParseQuery < ParseObject > query = ParseObject.GetQuery("UserInformation");
-							   
-							string usernameL = usernameText.Text;
-							string usernameR = userInfo.Get<string>("Username");
+							ParseQuery<ParseObject> query = from userInformation in ParseObject.GetQuery("UserInformation")
+														   where userInformation.Get<string>("Username") == usernameText.Text
+														   && userInformation.Get<string>("Password") == passwordText.Text
+														   select userInformation;
+							
+							ParseObject result = await query.FirstAsync();
+							int vouchers = result.Get<int>("Vouchers");
+							orderViewController.GetVouchers = vouchers;
 
-
-						   	string passwordL = passwordText.Text;
-						   	string passwordR = userInfo.Get<string>("Password");
-
-						   if (usernameL.Equals(usernameR) && passwordL.Equals(passwordR))
-						   {
-							   NavigationScreenController(orderViewController);
-							   orderViewController.GetVouchers = userInfo.Get<int>("Vouchers");
-						   }
-						   else if (i == 3)
-						   {
-
-							   AlertPopUp("Login failed!!", "You failed to login 3 times we suggest \nyou either Register or retrieve lost password", "OK");
-							   BorderButton(registerButton, forgotPasswordButton);
-							   loginButton.Enabled = false;
-						   }
-						   else {
-
-							   AlertPopUp("Login failed!!", "Username and/or password is incorrect", "OK");
+							NavigationScreenController(orderViewController);						
 
 						   }
+						   catch (ParseException) {
+							   AlertPopUp("Login failed","Username or password incorrect","OK");
 
+							   if (i == 3)
+							   {
 
-					   }
-					   catch (IndexOutOfRangeException)
-					   {
-						   //Do nothing this is an empty value returned. The user did not register
-					   }
+								   AlertPopUp("Login failed!!", "You failed to login 3 times we suggest \nyou either Register or retrieve lost password", "OK");
+								   BorderButton(registerButton, forgotPasswordButton);
+								   loginButton.Enabled = false;
+							   }
+							}
 
-
-*/
 					   break;
 
 				   case false:
--- iOS/Views/RegisterViewController.cs
+++ iOS/Views/RegisterViewController.cs
@@ -1,10 +1,21 @@
 ﻿
+using Parse;
+using UIKit;
+
 namespace HookMeUP.iOS
 {
 	public partial class RegisterViewController : ScreenViewController
 	{
-		
 
+		void SetTags() {
+			nameText.Tag = 0;
+			surnameText.Tag = 1;
+			usernameTextR.Tag = 2;
+			passwordTextR.Tag = 3;
+			verifyPasswordText.Tag = 4;
+			employeeNoText.Tag = 5;
+
+		}
 		public override void ViewDidLoad()
 		{
 			base.ViewDidLoad();
@@ -13,42 +24,54 @@ namespace HookMeUP.iOS
 			NavigationController.NavigationBarHidden = true;
 			DismissKeyboardOnBackgroundTap();
 			RegisterForKeyboardNotifications();
+			nameText.BecomeFirstResponder();
+			SetTags();
 
+			nameText.ShouldReturn += (textField) => {
+				int nextTag = (int)textField.Tag + 1;
+				UIResponder nextResponder = ((UITextField)textField).Superview.ViewWithTag(nextTag);
+				if (nextResponder.BecomeFirstResponder())
+				{
+					nextResponder.BecomeFirstResponder();
+				}
+				else
+				{
+					((UITextField)textField).ResignFirstResponder();
+				}
+				return true;
+			};
+		
 
 			submitButton.TouchUpInside += (sender, evt) => {
-				
-			
-					string name = nameText.Text;
-					string surname = surnameText.Text;
-					string username = usernameTextR.Text;
-					string password = passwordTextR.Text;
-					string empNo = employeeNoText.Text;
-
-					if (!name.Equals("") && !surname.Equals("") && !username.Equals("") && !password.Equals("") && !verifyPasswordText.Text.Equals("") && !empNo.Equals(""))
-					{
-						if (password.Equals(verifyPasswordText.Text))
-						{
-							//info.Add(name + "#" + surname + "#" + username + "#" + password + "#" + empNo+"#2");
 							
+				string name = nameText.Text;
+				string surname = surnameText.Text;
+				string username = usernameTextR.Text;
+				string password = passwordTextR.Text;
+				string empNo = employeeNoText.Text;
 
-							AddToDB(name, surname, username, password, empNo,23);
+				if (!name.Equals("") && !surname.Equals("") && !username.Equals("") && !password.Equals("") && !verifyPasswordText.Text.Equals("") && !empNo.Equals(""))
+				{
+					if (password.Equals(verifyPasswordText.Text))
+					{
+						//info.Add(name + "#" + surname + "#" + username + "#" + password + "#" + empNo+"#2");
+							
+						AddToDB(name, surname, username, password, empNo,23);
+						NavigationController.PopViewController(true);
+						AlertPopUp("Done!!!", "Registration complete", "OK");
+						ClearFields(nameText,surnameText,usernameTextR,passwordTextR,verifyPasswordText,employeeNoText);
+						isRegisteredSuccessful = true;
 
-							NavigationController.PopViewController(true);
-							AlertPopUp("Done!!!", "Registration complete", "OK");
-							ClearFields(nameText,surnameText,usernameTextR,passwordTextR,verifyPasswordText,employeeNoText);
-							isRegisteredSuccessful = true;
-
-						}
-						else { 
-						
-							AlertPopUp("Registering failed!!!", "passwords do not match", "OK");
-
-							passwordTextR.Highlighted = true;
-							verifyPasswordText.Highlighted = true;
-							isRegisteredSuccessful = false;
-						}
-						
 					}
+					else { 
+
+						AlertPopUp("Registering failed!!!", "passwords do not match", "OK");
+						passwordTextR.Highlighted = true;
+						verifyPasswordText.Highlighted = true;
+						isRegisteredSuccessful = false;
+						}
+					}
+
 				else {
 
 					AlertPopUp("Error","please complete all fields","OK","Cancel");
@@ -84,6 +107,8 @@ namespace HookMeUP.iOS
 
 
 	}
+
+
 
 
 }
