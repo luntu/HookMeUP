44df8cab662a35e47c719c94170bac113c5f9946
Luntu <luntu.mavukz35@gmail.com>
2016-08-10 09:21:26 +0200

    loading status bar working

--- iOS/Views/RegisterViewController.cs
+++ iOS/Views/RegisterViewController.cs
@@ -2,26 +2,36 @@
 using UIKit;
 using Parse;
 using ToastIOS;
+using System;
+using CoreGraphics;
 
 namespace HookMeUP.iOS
 {
 	public partial class RegisterViewController : ScreenViewController
 	{
+		LoadingOverlay loadingOverlay;
 		List<UITextField> fields = new List<UITextField>();
+		string values;
+	
 		async void Values() {
+
+			var bounds = UIScreen.MainScreen.Bounds;
+			loadingOverlay = new LoadingOverlay(bounds);
+			View.Add(loadingOverlay);
 
 			ParseQuery<ParseObject> query = ParseObject.GetQuery("UserInformation");
 			query.Include("Username");
 
 			var coll = await query.FindAsync();
 
-			string value;
+
 
 			foreach (ParseObject element in coll)
 			{
-				value = element.Get<string>("Username")+"\n";
+				values = element.Get<string>("Username")+"\n";
 
 			}
+			loadingOverlay.Hide();
 		
 		}
 
@@ -43,6 +53,7 @@ namespace HookMeUP.iOS
 			emailText.Tag = 5;
 
 		}
+
 		public override void ViewDidLoad()
 		{
 			base.ViewDidLoad();
@@ -68,37 +79,23 @@ namespace HookMeUP.iOS
 					return true;
 				};
 
-
 			}
 		
-				//			usernameTextR.AllEvents += async(sender, e) => {
-				//				try
-				//				{
-				//					ParseQuery<ParseObject> query = ParseObject.GetQuery("UserInformation");
-				////													where userInfo.Get<string>("Username") == usernameTextR.Text
+				usernameTextR.TouchCancel += (sender, e) => {
 
-				////				                                                            select userInfo;
-				//					query.Include("Username");
-				//					var coll =await query.FindAsync();
-
-				//					foreach (ParseObject element in coll) {
-				//						string value = element.Get<string>("Username");
-				//						System.Diagnostics.Debug.WriteLine(value);
-				//					} 
-				//					//ParseObject obj = await query.FirstAsync();
-				//					//if(obj.Get<string>("Username").Equals(usernameTextR.Text))
-				//					//Toast.MakeText("Someone already has that username ").Show();
-				//					submitButton.Enabled = false;
-				//				}
-				//				catch (ParseException) {
-				//					submitButton.Enabled = true;
-				//				}
-
-				//			};
+					
+								
+				};
 
 
 				submitButton.TouchUpInside += (sender, evt) => {
-							
+
+					if (values.Contains(usernameTextR.Text))
+					{
+					Toast.MakeText("Username already available").Show();
+					}
+					else Toast.MakeText("none").Show();
+
 				string name = nameText.Text;
 				string surname = surnameText.Text;
 				string username = usernameTextR.Text;
@@ -163,7 +160,65 @@ namespace HookMeUP.iOS
 
 	}
 
+	public class LoadingOverlay : UIView
+	{
+		// control declarations
+		UIActivityIndicatorView activitySpinner;
+		UILabel loadingLabel;
 
+		public LoadingOverlay(CGRect frame) : base(frame)
+		{
+			// configurable bits
+
+			Alpha = 0.75f;
+			AutoresizingMask = UIViewAutoresizing.All;
+
+			nfloat labelHeight = 22;
+			nfloat labelWidth = Frame.Width - 20;
+
+			// derive the center x and y
+			nfloat centerX = Frame.Width / 2;
+			nfloat centerY = Frame.Height / 2;
+
+			// create the activity spinner, center it horizontall and put it 5 points above center x
+			activitySpinner = new UIActivityIndicatorView(UIActivityIndicatorViewStyle.WhiteLarge);
+			activitySpinner.Frame = new CGRect(
+				centerX - (activitySpinner.Frame.Width / 2),
+				centerY - activitySpinner.Frame.Height - 20,
+				activitySpinner.Frame.Width,
+				activitySpinner.Frame.Height);
+			activitySpinner.AutoresizingMask = UIViewAutoresizing.All;
+			AddSubview(activitySpinner);
+			activitySpinner.StartAnimating();
+			activitySpinner.Color = UIColor.Black;
+			// create and configure the "Loading Data" label
+			loadingLabel = new UILabel(new CGRect(
+				centerX - (labelWidth / 2),
+				centerY,
+				labelWidth,
+				labelHeight
+				));
+			loadingLabel.BackgroundColor = UIColor.Clear;
+			loadingLabel.TextColor = UIColor.Black;
+			loadingLabel.Text = "Loading Data...";
+			loadingLabel.TextAlignment = UITextAlignment.Center;
+			loadingLabel.AutoresizingMask = UIViewAutoresizing.All;
+			AddSubview(loadingLabel);
+
+		}
+
+		/// <summary>
+		/// Fades out the control and then removes it from the super view
+		/// </summary>
+		public void Hide()
+		{
+			UIView.Animate(
+				0.5, // duration
+				() => { Alpha = 0; },
+				() => { RemoveFromSuperview(); }
+			);
+		}
+	}
 
 
 }
