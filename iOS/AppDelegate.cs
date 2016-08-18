5889cc876e491315749bebf20bf56c89fd0ffe1d
Luntu <luntu.mavukz35@gmail.com>
2016-08-18 12:13:39 +0200

    iPhone and iPad screens corresponding

--- iOS/AppDelegate.cs
+++ iOS/AppDelegate.cs
@@ -22,9 +22,6 @@ namespace HookMeUP.iOS
 			// If not required for your application you can safely delete this method
 
 			// Code to start the Xamarin Test Cloud Agent
-//#if ENABLE_TEST_CLOUD
-//			Xamarin.Calabash.Start();
-//#endif
 
 			this.Window = new UIWindow(UIScreen.MainScreen.Bounds);
 			UIViewController loginController = new LoginViewController();
